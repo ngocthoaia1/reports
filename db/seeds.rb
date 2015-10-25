91e27ba534e285863e696e30bb09eb660b93e3ce
Ngoc Thoai Nguyen <ngocthoaia1@gmail.com>
2015-10-25 19:22:34 +0700

    ajax-datatables-rails common

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create!(name: 'User1', phone: '111111111', address: "xyz xyz xyz")
+User.create!(name: 'User2', phone: '121212121', address: "zyx zyx zyx")
+User.create!(name: 'User3', phone: '131313131', address: "zyx zyx zyx")
+User.create!(name: 'User4', phone: '141414141', address: "zyx zyx zyx")
+User.create!(name: 'User5', phone: '151515151', address: "zyx zyx zyx")
+User.create!(name: 'User6', phone: '161616161', address: "zyx zyx zyx")
